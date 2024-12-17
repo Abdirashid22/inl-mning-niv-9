25dff822f7b27de71cc1375871c0aa65d1d2e0f1
absa07002 <absa07002@DESKTOP-TAGHD5A>
2024-12-17 14:14:21 +0100

    Add project files.

---
+++ inlämning nivå 9/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ange antalet rader för triangeln: ");
+        int rader = int.Parse(Console.ReadLine());
+
+        
+        for (int i = 1; i <= rader; i++)
+        {
+            
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write("*"); 
+            }
+            Console.WriteLine(); 
+        }
+    }
+}
+
